63dda9aa0bab65adf5185236ea181dc4438f93e9
aoi0412 <aoiaoi0412.1203@gmail.com>
2023-02-19 17:29:03 +0900

    APIがちゃんと叩けるかのテスト

--- src/pages/api/getPrefList.ts
+++ src/pages/api/getPrefList.ts
@@ -11,38 +11,47 @@ type Props = {
 export const getPrefList = (result: Props) => {
   const url =
     'https://opendata.resas-portal.go.jp/api/v1/prefectures'
-  return axios
-    .get(url, {
-      headers: {
-        'X-API-KEY': process.env.REACT_APP_RESAS_API_KEY,
-      },
-    })
-    .then((res: AxiosResponse<apiResult<prefData[]>>) => {
-      if (res.status !== 200) {
-        result.ifError({
-          statusCode: `${res.status}`,
-          message: res.statusText,
-          description: 'APIがうまく動きませんでした',
-        })
-      } else if (res.data.statusCode) {
-        result.ifError({
-          statusCode: res.data.statusCode,
-          message: res.data.message,
-          description: res.data.description
-            ? res.data.description
-            : '',
-        })
-      }
-      if (res.data.result) {
-        result.ifSuccess(res.data.result)
-      }
-    })
-    .catch((e: AxiosError<{ error: string }>) => {
-      ifError({
-        statusCode: e.code ? e.code : '',
-        message: e.message,
-        description: '',
+  if (process.env.NEXT_PUBLIC_RESAS_API_KEY) {
+    return axios
+      .get(url, {
+        headers: {
+          'X-API-KEY':
+            process.env.NEXT_PUBLIC_RESAS_API_KEY,
+        },
       })
+      .then((res: AxiosResponse<apiResult<prefData[]>>) => {
+        if (res.status !== 200) {
+          result.ifError({
+            statusCode: `${res.status}`,
+            message: res.statusText,
+            description: 'APIがうまく動きませんでした',
+          })
+        } else if (res.data.statusCode) {
+          result.ifError({
+            statusCode: res.data.statusCode,
+            message: res.data.message,
+            description: res.data.description
+              ? res.data.description
+              : '',
+          })
+        }
+        if (res.data.result) {
+          result.ifSuccess(res.data.result)
+        }
+      })
+      .catch((e: AxiosError<{ error: string }>) => {
+        ifError({
+          statusCode: e.code ? e.code : '',
+          message: e.message,
+          description: '',
+        })
+      })
+      .finally(result.after)
+  } else {
+    result.ifError({
+      statusCode: '',
+      message: 'APIキーが正しくありません',
+      description: '正しいAPIキーを設定してください',
     })
-    .finally(result.after)
+  }
 }
